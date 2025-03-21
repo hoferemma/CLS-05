75c1e49c99f6515b9e0283128afb0cb71608b8ce
Emma Hofer <hoferlemma@gmail.com>
2025-03-20 20:37:46 -0500

    modified base code, needs to be improved. have created an initial submission.csv based off preliminary results of base code.

--- cls_05_code.py
+++ cls_05_code.py
@@ -3,11 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import nltk
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn import feature_extraction, linear_model, model_selection, preprocessing
 
-train_df = pd.read_csv("train.csv")
-test_df = pd.read_csv("test.csv")
+# read csv files
+train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv")
 
+# building our vectors (count words in each tweet and turn into data)
+count_vectorizer = feature_extraction.text.CountVectorizer()
+
+train_vectors = count_vectorizer.fit_transform(train_df["text"])
+test_vectors = count_vectorizer.transform(test_df["text"])
+
+#build a linear model
+clf = linear_model.RidgeClassifier()
+
+#use cross validation to test our model 
+scores = model_selection.cross_val_score(clf, train_vectors, train_df['target'], cv=3, scoring="f1")
+
+# predictions on training set
+clf.fit(train_vectors, train_df["target"])
+sample_submission = pd.read_csv("data/sample_submission.csv")
+sample_submission["target"] = clf.predict(test_vectors)
+
+sample_submission.head()
+sample_submission.to_csv("submission.csv", index=False)
