47399d7d4089bf83b6243047ef9e6a02bd867366
Emma Hofer <hoferlemma@gmail.com>
2025-04-12 15:30:59 -0500

    edited code to export to new csv file, titled for submission. also defined function for cleaning up text prior to building vectorizers

--- cls_05_code.py
+++ cls_05_code.py
@@ -8,12 +8,31 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re 
 import nltk
+from nltk.corpus import stopwords
 from sklearn import feature_extraction, linear_model, model_selection, preprocessing
+
+#download nltk stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words("english"))
+
+#clean tweet text: remove urls, punctuation, lowercase, stopwords
+def clean_text(text):
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^A-Za-z\s]", "", text) 
+    text = text.lower()
+    tokens = text.split()
+    tokens = [word for word in tokens if word not in stop_words]
+    return " ".join(tokens)
 
 # read csv files
 train_df = pd.read_csv("data/train.csv")
 test_df = pd.read_csv("data/test.csv")
+
+# apply text cleaning
+train_df["clean_text"] = train_df["text"].apply(clean_text)
+test_df["clean_text"] = test_df["text"].apply(clean_text)
 
 # building our vectors (count words in each tweet and turn into data)
 count_vectorizer = feature_extraction.text.CountVectorizer()
@@ -33,4 +52,4 @@ sample_submission = pd.read_csv("data/sample_submission.csv")
 sample_submission["target"] = clf.predict(test_vectors)
 
 sample_submission.head()
-sample_submission.to_csv("submission.csv", index=False)
+sample_submission.to_csv("CLS-05-CKPT2.csv", index=False)
